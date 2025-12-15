b96b1e45bb6ad3c869c149f2f5f61007902e92b9
balajioffl <balajioffl07@gmail.com>
2025-12-15 15:10:51 +0530

    notification added

--- myproject/notifications/consumers.py
+++ myproject/notifications/consumers.py
@@ -1,6 +1,38 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
+import json
 
 class NotificationConsumer(AsyncWebsocketConsumer):
+
     async def connect(self):
-        print("WEBSOCKET CONNECTED")
+        self.group_name = "notifications"
+
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+
         await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        message = data.get("message")
+
+        await self.channel_layer.group_send(
+            self.group_name,
+            {
+                "type": "send_notification",
+                "message": message
+            }
+        )
+
+    async def send_notification(self, event):
+        await self.send(text_data=json.dumps(
+        {
+            "message": event["message"]
+        }))
---
+++ readme.txt
@@ -0,0 +1,1 @@
+to run - daphne -b 127.0.0.1 -p 8000 myproject.asgi:application
